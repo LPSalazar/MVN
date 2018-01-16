ae1da9e062567cfbdf9f6172883c262c2dc67b70
LPSalazar <35040690+LPSalazar@users.noreply.github.com>
2018-01-16 10:41:21 -0300

    script
    ejecuta maven

--- scripts/mvnContruir.sh
+++ scripts/mvnContruir.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/proyectos/EC2yCodeDeploy2/target
-java -jar EC2yCodeDeploy2-0.0.1-SNAPSHOT.jar
+cd /home/ec2-user/PruebaMvn/mvnDeEC2
+mvn compile
+mvn package
+cd /home/ec2-user/PruebaMvn/mvnDeEC2/target
+java -jar mvnDeEC2-0.0.1-SNAPSHOT.jar
