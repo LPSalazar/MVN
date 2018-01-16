cb8d6435b9a95886acd158cedff86fbe4c9846a3
LPSalazar <35040690+LPSalazar@users.noreply.github.com>
2018-01-16 13:06:34 -0300

    script2
    se actualizo java en ec2

--- scripts/mvnContruir.sh
+++ scripts/mvnContruir.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export MAVEN_HOME=/opt/apache-maven-3.5.2
+
+export M2_HOME=/opt/apache-maven-3.5.2
+export PATH=$PATH:$M2_HOME/bin 
+
 cd /home/ec2-user/PruebaMvn/mvnDeEC2
 mvn compile
 mvn package
